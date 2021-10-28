d149101ab39fed3e22480b553c75a6ea8cdcc75a
“kkjos” <k19951227@gmail.com>
2021-10-28 04:06:20 +0000

    [finish]chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7fdd8cbc18ff6aeff7e086db4b2792d61d6b57794abbddca6461912c320c890e7b25fc2a59af95aa3b58393c1cf05e37a276d3151d189385a2eef4949097e51d'
